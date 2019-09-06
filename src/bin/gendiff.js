d79c678a9c02aabab7e52c53e8f6ac96e4fc30ed
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-09-06 12:20:46 +0000

    Step 1 init

---
+++ src/bin/gendiff.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program.version('1.0.0');
+program.description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
