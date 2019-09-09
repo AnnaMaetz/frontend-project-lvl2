e7457255a8ab62367dc783c788649b132b94dac3
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-09-09 14:09:27 +0000

    Update code step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 import program from '..';
 
-let firstConfigValue;
-let secondConfigValue;
-
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    firstConfigValue = firstConfig;
-    secondConfigValue = secondConfig;
-  });
-
-program.parse(process.argv);
+    console.log(firstConfig, secondConfig);
+  })
+  .parse(process.argv);
