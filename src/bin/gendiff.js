cc3ccd0d09f6669a33ba4cd839ea28d69d411cb3
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-09-17 11:11:15 +0000

    Step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
-import program from '..';
+import commander from 'commander';
+import genDiff from '..';
 
+const program = new commander.Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    console.log(firstConfig, secondConfig);
+    const diff = genDiff(firstConfig, secondConfig);
+    console.log(diff);
   })
   .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import commander from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 
-export default new commander.Command();
+const readFile = (pathToFile) => {
+  const fd = fs.openSync(pathToFile);
+  const contentFile = JSON.parse(fs.readFileSync(fd, 'utf8'));
+  fs.closeSync(fd);
+  return contentFile;
+};
+
+export default (pathToFile1, pathToFile2) => {
+  const contentFile1 = readFile(pathToFile1);
+  const contentFile2 = readFile(pathToFile2);
+
+  const resultFile1 = _.reduce(contentFile1, (result, value, key) => {
+    if (_.has(contentFile2, key)) {
+      if (value === contentFile2[key]) {
+        return result.concat(`    ${key}: ${value}\n`);
+      }
+      return result.concat(`  + ${key}: ${contentFile2[key]}\n  - ${key}: ${value}\n`);
+    }
+    return result.concat(`  - ${key}: ${value}\n`);
+  }, '{ \n');
+
+  const resultFile2 = _.reduce(contentFile2, (result, value, key) => {
+    if (!_.has(contentFile1, key)) {
+      return result.concat(`  + ${key}: ${value}\n`);
+    }
+    return result;
+  }, resultFile1).concat('}');
+
+  return resultFile2;
+};
