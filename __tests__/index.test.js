e3c9289ccedd605f036ffaa7f8999681855dbc66
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-09-19 09:53:01 +0000

    Update .eslintrc.yml __tests__/index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import genDiff from '../src';
+
+test('genDiff', () => {
+  const result = '{/n    host: hexlet.io/n  + timeout: 20/n  - timeout: 50/n  - proxy: 123.234.53.22/n  - follow: false/n  + verbose: true/n}';
+
+  expect(genDiff('before.json', 'after.json')).toEqual(result);
+});
